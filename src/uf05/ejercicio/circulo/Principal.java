ba78b1ad6def6ae50165c454ad4d67f4eaf05de5
Alex <alexr.h67@gmail.com>
2020-05-24 14:35:26 +0200

    Version Final

--- src/uf05/ejercicio/circulo/Principal.java
+++ src/uf05/ejercicio/circulo/Principal.java
@@ -25,14 +25,50 @@ public class Principal {
             }            
         }while(radio>=0.1);
         
+        int numero = pregunta();
+        ordenarLista(numero);
+        imprimirLista();
+        
     }
     
     public double radioRandom(){
         return Math.random();
     }
     
-  
+    public int pregunta(){
+        Scanner scanner = new Scanner(System.in);
+        int respuesta = 0;
+        boolean respuestaOK = false;
+        do{
+            System.out.println("Ordenad de manera ascendente(1) o descendente(2)?");
+            if(scanner.hasNextInt()){
+                respuesta = scanner.nextInt();
+                if(respuesta == 1 || respuesta == 2){
+                    respuestaOK = true;
+                }else{
+                    System.out.println("Introduzca un numero valido");
+                }
+            }else{
+                System.out.println("Por favor introduzca 1 o 2");
+                scanner.next();
+            }
+        }while(!respuestaOK);
+        return respuesta;
+    }
+    
+    
+    public void ordenarLista(int numero){
+        if(numero == 1){
+            Collections.sort(arraycirculo);
+        }else{
+            Collections.sort(arraycirculo, Collections.reverseOrder());
+        }
+    }
+    
+    public void imprimirLista(){
+        for(Circulo circulo : arraycirculo){
+            System.out.println(circulo); 
+        }
+    }
     
 }
-
-
